54af3151882a2a2c690c9ef1193065f68a9abf28
MutaiAllan <allankiprop175@gmail.com>
2024-03-27 10:12:17 +0300

    Addedmodel

---
+++ airline/flights/urls.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path("", views.index, name="index")
+]
